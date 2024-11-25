d0f331c75dfc3dd12513974d87e8c841983c0c0c
Arman <mdarman365506@gmail.com>
2024-11-25 15:09:04 +0600

    fetch data from server

--- index.js
+++ index.js
@@ -27,10 +27,23 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
+        //   const database = client.db("usersDB");
+      //   const usersCollection = database.collection("users");
+    const usersCollection = client.db('usersDB').collection('users');
+    
+
+    app.get('/users', async (req, res) => {
+      const cursor = usersCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
 
       app.post('/users', async (req, res) => {
           const user = req.body;
           console.log('New User', user);
+          const result = await usersCollection.insertOne(user);
+          res.send(result)
       });
 
 
