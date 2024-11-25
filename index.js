45c4ac576784fdbb65dc5bdac794143bf7febcaf
Arman <mdarman365506@gmail.com>
2024-11-25 15:44:09 +0600

    deleted from mongodb by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -45,6 +45,14 @@ async function run() {
           const result = await usersCollection.insertOne(user);
           res.send(result)
       });
+    
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log('Please delete form database', id);
+      const query = { _id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
